83b897f45e06c81041ea0e13c35577076e872dc6
nileshdhepale <nildhepale997@gmail.com>
2025-07-15 17:15:57 +0530

    added vercel file

--- server.js
+++ server.js
@@ -29,13 +29,24 @@ app.use("/api/users", userRoutes);
 
 // Routes will go here later
 
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//   })
+//   .catch((err) => console.error("DB connection failed:", err));
+
+// Connect to MongoDB only once
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("DB connection failed:", err));
+
+module.exports = app;
