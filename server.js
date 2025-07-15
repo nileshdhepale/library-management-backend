9133cf0f6763f98528e12dd3462e7cda5f984805
nileshdhepale <nildhepale997@gmail.com>
2025-07-15 18:32:07 +0530

    Added the cors

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or frontend domain for security
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
